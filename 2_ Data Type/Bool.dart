a273c6c75443edf5e525fdf4113c0ecfaa6d3c8b
Md Hafiz <smhafizofficial@gmail.com>
2025-11-03 03:14:02 +0600

    Practice bool Datatype

---
+++ 2_ Data Type/Bool.dart
@@ -0,0 +1,14 @@
+//Boolean - একটি True / False Datatype. এটির বিট সংখা হল ২।
+
+void main() {
+  bool isTrue = true;
+  var isFalse = false;
+
+  print("Hey mr. 5 != 10: $isTrue");
+  print("Hey mr. 5 == 10: $isFalse");
+}
+
+
+/* বর্ণনা: bool ডেটা টাইপটি বুলিয়ান মান ধারণ করে, অর্থাৎ true অথবা false।
+   bool টাইপের ভেরিয়েবলগুলি সাধারণত শর্তাধীন লজিক এবং নিয়ন্ত্রণ স্টেটমেন্টে ব্যবহৃত হয়।
+   এটি Boolean লজিকের জন্য ব্যবহার করা হয়, যেমন if এবং while শর্ত। */
