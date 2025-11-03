3af23071d4d1a330a6420ac29a65228d88cd463a
Md Hafiz <smhafizofficial@gmail.com>
2025-11-04 05:47:48 +0600

    practice assignment operator

--- 3_ Operator/assignment.dart
+++ 3_ Operator/assignment.dart
@@ -1,12 +1,12 @@
 // অ্যাসাইনমেন্ট অপারেটরস (Assignment Operators)
 // এই অপারেটরগুলো ভেরিয়েবলে মান অ্যাসাইন করতে ব্যবহৃত হয়।
 
-void main(){
+void main() {
   int a = 10;
   int b = 5;
-  a += b;  // a = a + b ->15
-  a -= b;  // a = a - b -> 10
-  a *= b;  // a = a * b -> 50
-  a = b;  // a = a / b -> 10.0
-  a %= b;  // a = a % b -> 0
+  print("addition is: ${a += b}"); // a = a + b ->15
+  print("substraction is: ${a -= b}"); // a = a - b -> 10
+  print("multiplication is: ${a *= b}"); // a = a * b -> 50
+  print("division is: ${a = b}"); // a = a / b -> 10.0
+  print("modulus is: ${a %= b}"); // a = a % b -> 0
 }
