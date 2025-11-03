4fe575d712ad3ca30215dd96dd53cb892685751c
Md Hafiz <smhafizofficial@gmail.com>
2025-11-04 05:48:38 +0600

    Practice Relational Operator

--- 3_ Operator/relational.dart
+++ 3_ Operator/relational.dart
@@ -4,10 +4,10 @@
 void main(){
   int a = 10;
   int b = 5;
-  print(a == b);  // false
-  print(a != b);  // true
-  print(a > b);   // true
-  print(a < b);   // false
-  print(a >= b);  // true
-  print(a <= b);  // false
+  print(a == b);   // false
+  print(a != b);   // true
+  print(a > b);    // true
+  print(a < b);    // false
+  print(a >= b);   // true
+  print(a <= b);   // false
 }
