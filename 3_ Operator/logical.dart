0f448095b84c87e9e08d41c2828ef3df079f11ab
Md Hafiz <smhafizofficial@gmail.com>
2025-11-04 06:02:58 +0600

    practice logical operator

--- 3_ Operator/logical.dart
+++ 3_ Operator/logical.dart
@@ -1,10 +1,14 @@
 // লজিক্যাল অপারেটরস (Logical Operators)
 // এই অপারেটরগুলো শর্তের মধ্যে লজিক্যাল সিদ্ধান্ত নিতে ব্যবহৃত হয়।
 
-void main(){
+void main() {
   bool a = true;
   bool b = false;
-  print(a && b);  // false
-  print(a || b);  // true
-  print(!a);      // false
+  var x = 29;
+  var y = 15;
+  print(a && b);
+  if (x > y || y < x) {
+    print("True");
+  }
+  print(!a); // false
 }
